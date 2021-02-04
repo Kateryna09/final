8a9f0f5caa6a1b8ddff80a1d899acc550d005dcd
meden <medenets.kat@gmail.com>
2021-02-04 18:35:41 +0300

    add project

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:0,
@@ -15,4 +15,36 @@ $(document).ready(function(){
             }
         }
     });
+$(document).ready(function(){
+    var $element = $('.work');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        var offset = $element.offset().top
+
+        if (scroll > offset && counter == 0) {
+                //$(".number").countUp({delay:10,time:1000})
+                var options = {
+                    useEasing: true,
+                    useGrouping: true,
+                    separator: ',',
+                    decimal: '.',
+                    prefix: '',
+                    suffix: ''
+                };
+                var demo = new CountUp("number", 0, 420, 0, 2.5, options);
+                demo.start();
+                var demo = new CountUp("number2", 0, 310, 0, 2.5, options);
+                demo.start();
+                var demo = new CountUp("number3", 0, 720, 0, 2.5, options);
+                demo.start();
+                var demo = new CountUp("number1", 0, 990, 0, 2.5, options);
+                demo.start();
+            counter++;
+        }
+    });
+
 });
